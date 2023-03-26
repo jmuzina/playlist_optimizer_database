e59373099c9a7fe062309fa740643a5a49006caa
jmuzina <joe.muzina@gmail.com>
2023-03-26 19:24:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Playlist Optimizer (Postgres Database)
+
+Playlist Optimzer is an open-source web application that allows users to analyze their music listening preferences and create playlists accordingly. 
+
+It was born out of a frequent issue I face; skipping song after song in Spotify playlists. When you have hundreds of songs in a playlist, it becomes overwhelming to remove songs you don't want to listen to anymore.
+
+It is a modern re-imagining of [Spotify Playlist Optimizer](https://github.com/jmuzina/spotify_playlist_optimizer), a naïve implementation I made when I was still very new to programming. With the skills I've gained since graduating and getting into industry, I'm looking forward to making this app more modern, performant, and intuitive!
+
+The legacy version is accessible at [https://joemuzina.com:8080](https://joemuzina.com:8080).
+
+------------------
+## Planned features
+
+* **Authenticate**: Authenticate with a variety of music services (Spotify at first, then others if feasible).
+* **View your top tracks**: See a list of songs you've been listening to the most over a customizable time frame.
+* **Create playlists**: Create new playlists using your top tracks.
+* **Compare playlists**: Compare existing playlists with your top tracks.
+* **Optimze playlists**: Add, remove, or keep songs to existing playlists based on your top tracks.
+------------------
+## Architecture
+* **Frontend**: An [Angular web app](https://github.com/jmuzina/playlist_optimizer_frontend) for direct user interface.
+* **API**: A [Hasura GraphQL Engine](https://github.com/jmuzina/playlist_optimizer_api) instance handles user requests, permissions, and forwards them to the back-end.
+* **Backend**: A [NodeJS server](https://github.com/jmuzina/playlist_optimizer_backend) interfaces with music listening service APIs to authenticate users, view their top tracks & playlists, and manipulate playlists.
+* **Database**: A [PostgresSQL server](https://github.com/jmuzina/playlist_optimizer_database) database.
+------------------
---
+++ data/schema.sql
@@ -0,0 +1,132 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.2 (Debian 15.2-1.pgdg110+1)
+-- Dumped by pg_dump version 15.2 (Debian 15.2-1.pgdg110+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: playlist_optimizer; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+CREATE SCHEMA playlist_optimizer;
+
+
+ALTER SCHEMA playlist_optimizer OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: user; Type: TABLE; Schema: playlist_optimizer; Owner: postgres
+--
+
+CREATE TABLE playlist_optimizer."user" (
+    id integer NOT NULL
+);
+
+
+ALTER TABLE playlist_optimizer."user" OWNER TO postgres;
+
+--
+-- Name: User_id_seq; Type: SEQUENCE; Schema: playlist_optimizer; Owner: postgres
+--
+
+CREATE SEQUENCE playlist_optimizer."User_id_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE playlist_optimizer."User_id_seq" OWNER TO postgres;
+
+--
+-- Name: User_id_seq; Type: SEQUENCE OWNED BY; Schema: playlist_optimizer; Owner: postgres
+--
+
+ALTER SEQUENCE playlist_optimizer."User_id_seq" OWNED BY playlist_optimizer."user".id;
+
+
+--
+-- Name: spotify_user; Type: TABLE; Schema: playlist_optimizer; Owner: postgres
+--
+
+CREATE TABLE playlist_optimizer.spotify_user (
+    id text NOT NULL,
+    u_id_fk integer NOT NULL
+);
+
+
+ALTER TABLE playlist_optimizer.spotify_user OWNER TO postgres;
+
+--
+-- Name: v_users; Type: VIEW; Schema: playlist_optimizer; Owner: postgres
+--
+
+CREATE VIEW playlist_optimizer.v_users AS
+ SELECT usr.id AS u_id,
+    spotifyuser.id AS spotify_u_id
+   FROM (playlist_optimizer."user" usr
+     RIGHT JOIN playlist_optimizer.spotify_user spotifyuser ON ((usr.id = spotifyuser.u_id_fk)));
+
+
+ALTER TABLE playlist_optimizer.v_users OWNER TO postgres;
+
+--
+-- Name: user id; Type: DEFAULT; Schema: playlist_optimizer; Owner: postgres
+--
+
+ALTER TABLE ONLY playlist_optimizer."user" ALTER COLUMN id SET DEFAULT nextval('playlist_optimizer."User_id_seq"'::regclass);
+
+
+--
+-- Name: spotify_user SpotifyUser_pkey; Type: CONSTRAINT; Schema: playlist_optimizer; Owner: postgres
+--
+
+ALTER TABLE ONLY playlist_optimizer.spotify_user
+    ADD CONSTRAINT "SpotifyUser_pkey" PRIMARY KEY (id);
+
+
+--
+-- Name: user User_pkey; Type: CONSTRAINT; Schema: playlist_optimizer; Owner: postgres
+--
+
+ALTER TABLE ONLY playlist_optimizer."user"
+    ADD CONSTRAINT "User_pkey" PRIMARY KEY (id);
+
+
+--
+-- Name: spotify_user user_unique_spotify_account; Type: CONSTRAINT; Schema: playlist_optimizer; Owner: postgres
+--
+
+ALTER TABLE ONLY playlist_optimizer.spotify_user
+    ADD CONSTRAINT user_unique_spotify_account UNIQUE (u_id_fk);
+
+
+--
+-- Name: spotify_user AppUser; Type: FK CONSTRAINT; Schema: playlist_optimizer; Owner: postgres
+--
+
+ALTER TABLE ONLY playlist_optimizer.spotify_user
+    ADD CONSTRAINT "AppUser" FOREIGN KEY (u_id_fk) REFERENCES playlist_optimizer."user"(id) NOT VALID;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
